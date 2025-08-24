5d41273f4174a94f2b3628569a192336a6d2bc53
Gage Gaskins <ggaskins93@gmail.com>
2025-08-24 00:52:13 -0700

    Add v1 route registrations with GatewayApplicationService

--- internal/gateway/api/router.go
+++ internal/gateway/api/router.go
@@ -4,15 +4,27 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/slackhq/spark-gateway/internal/gateway/api/health"
 	v1kubeflow "github.com/slackhq/spark-gateway/internal/gateway/api/v1/kubeflow"
+	"github.com/slackhq/spark-gateway/internal/gateway/service"
+	"github.com/slackhq/spark-gateway/internal/shared/config"
+	sgHttp "github.com/slackhq/spark-gateway/internal/shared/http"
 )
 
-func NewRouter(userService *service.UserService) {
+func NewRouter(sgConf *config.SparkGatewayConfig, appService service.GatewayApplicationService) *gin.Engine {
 
 	router := gin.Default()
+	router.Use(sgHttp.ApplicationErrorHandler)
 
 	// Root group for unversioned routes
 	rootGroup := router.Group("")
 
 	health.RegisterHealthRoutes(rootGroup)
+
+	// Versioned routes
+
+	v1Group := router.Group("/v1")
+
+	v1kubeflow.RegisterKubeflowApplicationRoutes(v1Group, sgConf, appService)
+
+	return router
 
 }
